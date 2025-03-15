8cb746356e940332e1b83d8015ae04835674e58c
Raul Costa <raulbrennersc@gmail.com>
2025-03-14 21:56:24 -0300

    add start container script and setup ssh server

---
+++ .devcontainer/scripts/start-container.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! -f already_ran ]; then
+  if [[ $CUSTOM_SSH_PORT ]]; then
+    sudo sudo sed -i 's/Port .*/Port '$CUSTOM_SSH_PORT'/' /etc/ssh/sshd_config
+  fi
+
+  if [[ $KEY_TO_AUTHORIZE ]]; then
+    mkdir -p .ssh
+    echo $KEY_TO_AUTHORIZE >>.ssh/authorized_keys
+    sudo sed -i 's/#AuthorizedKeysFile/AuthorizedKeysFile/' /etc/ssh/sshd_config
+  fi
+
+  touch already_ran
+
+fi
+
+sudo /etc/init.d/ssh start
